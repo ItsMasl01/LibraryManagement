65632fd7425d47ed675b0534d366d581d8dd37da
ItsMasl01 <meryamassemlali@gmail.com>
2024-12-19 23:18:22 +0100

    update host

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String URL = "jdbc:mysql://host.docker.internal:3306/library";
     private static final String USER = "root";
     private static final String PASSWORD = "Tunnina01*/";
     private static  Connection dbConnection ;
