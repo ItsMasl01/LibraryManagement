b396b60f81cb59cc39d1fef607f2c2f2a4595872
ItsMasl01 <meryamassemlali@gmail.com>
2024-12-19 23:50:38 +0100

    fix database properties

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -1,11 +1,13 @@
 package com.library.util;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Level;
 
 public class DbConnection {
     private static String URL;
@@ -13,35 +15,26 @@ public class DbConnection {
     private static String PASSWORD;
     private static Connection dbConnection;
 
-    static {
-        try (InputStream input = DbConnection.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (input == null) {
-                throw new RuntimeException("Unable to find application.properties in resources folder");
-            }
-            Properties properties = new Properties();
-            properties.load(input);
-
-            URL = properties.getProperty("db.url");
-            USER = properties.getProperty("db.user");
-            PASSWORD = properties.getProperty("db.password");
-        } catch (IOException ex) {
-            throw new RuntimeException("Failed to load database configuration", ex);
-        }
-    }
-
     private DbConnection() {
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
         try {
-            if (dbConnection != null && !dbConnection.isClosed()) {
+            if(dbConnection != null && !dbConnection.isClosed()) {
                 return dbConnection;
             }
+            Properties props = new Properties();
+            try (FileInputStream fis = new FileInputStream("config.properties")) {
+                props.load(fis);
+            }
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
             Class.forName("com.mysql.cj.jdbc.Driver");
             dbConnection = DriverManager.getConnection(URL, USER, PASSWORD);
             return dbConnection;
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new SQLException("Database connection error", e);
+        } catch (ClassNotFoundException | SQLException | IOException e) {
+            return null;
         }
     }
 }
